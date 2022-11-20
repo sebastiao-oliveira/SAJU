4f1b7b448c7301bb336dc184dad3e86460b7450f
SEBASTIAO DE OLIVEIRA MELO <54176273+sebastiao-oliveira@users.noreply.github.com>
2022-11-20 18:56:45 -0300

    first commit

---
+++ src/contexts/auth.js
@@ -0,0 +1,74 @@
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext({});
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    const userToken = localStorage.getItem("user_token");
+    const usersStorage = localStorage.getItem("users_bd");
+
+    if (userToken && usersStorage) {
+      const hasUser = JSON.parse(usersStorage)?.filter(
+        (user) => user.email === JSON.parse(userToken).email
+      );
+
+      if (hasUser) setUser(hasUser[0]);
+    }
+  }, []);
+ //Login
+  const signin = (cpf, password) => {
+    const usersStorage = JSON.parse(localStorage.getItem("users_bd"));
+
+    const hasUser = usersStorage?.filter((user) => user.cpf === cpf);
+
+    if (hasUser?.length) {
+      if (hasUser[0].cpf === cpf && hasUser[0].password === password) {
+        const token = Math.random().toString(36).substring(2);
+        localStorage.setItem("user_token", JSON.stringify({ cpf, token }));
+        setUser({ cpf, password });
+        return;
+      } else {
+        return "CPF ou senha incorretos";
+      }
+    } else {
+      return "Usuário não cadastrado";
+    }
+  };
+  //Cadastro
+  const signup = (cpf, password) => {
+    const usersStorage = JSON.parse(localStorage.getItem("users_bd"));
+
+    const hasUser = usersStorage?.filter((user) => user.cpf === cpf);
+
+    if (hasUser?.length) {
+      return "Já tem uma conta com esse CPF";
+    }
+
+    let newUser;
+
+    if (usersStorage) {
+      newUser = [...usersStorage, { cpf, password }];
+    } else {
+      newUser = [{ cpf, password }];
+    }
+
+    localStorage.setItem("users_bd", JSON.stringify(newUser));
+
+    return;
+  };
+  //Sair
+  const signout = () => {
+    setUser(null);
+    localStorage.removeItem("user_token");
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ user, signed: !!user, signin, signup, signout }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
---
+++ src/pages/Signin/index.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+import * as C from "./styles";
+import { Link, useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+
+//Tela login
+const Signin = () => {
+  const { signin } = useAuth();
+  const navigate = useNavigate();
+
+  const [cpf, setCPF] = useState("");
+  const [senha, setSenha] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = () => {
+    if (!cpf | !senha) {
+      setError("Preencha todos os campos");
+      return;
+    }
+
+    const res = signin(cpf, senha);
+
+    if (res) {
+      setError(res);
+      return;
+    }
+
+    navigate("/home");
+  };
+
+  return (
+    <C.Container>
+      <C.Label>SAJU</C.Label>
+      <C.Content>
+        <Input
+          type="cpf"
+          placeholder="Digite seu CPF"
+          value={cpf}
+          onChange={(e) => [setCPF(e.target.value), setError("")]}
+        />
+        <Input
+          type="password"
+          placeholder="Digite sua Senha"
+          value={senha}
+          onChange={(e) => [setSenha(e.target.value), setError("")]}
+        />
+        <C.labelError>{error}</C.labelError>
+        <Button Text="Entrar" onClick={handleLogin} />
+        <C.LabelSignup>
+          Não tem uma conta?
+          <C.Strong>
+            <Link to="/signup">&nbsp;Registre-se</Link>
+          </C.Strong>
+        </C.LabelSignup>
+      </C.Content>
+    </C.Container>
+  );
+};
+
+export default Signin;
